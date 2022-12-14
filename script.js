747ca1aa5f125e0dbfb622539b8eb3a088182416
bulofed <pro.butor.loic@gmail.com>
2022-12-14 13:35:22 +0100

    dark mode entre autres

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-document.getElementById("icon-mode").addEventListener("click", modeChange);
+document.getElementById("icon-mode2").addEventListener("click", modeChange);
 
 function modeChange() {
     let doc = document.getElementById("mode");
     let icon1 = document.getElementById("icon-mode1");
+    let icon2 = document.getElementById("icon-mode2");
     if (doc.className === "light") {
         doc.classList.replace("light", "dark");
-        icon1.classList.replace("light", "dark");
+        icon2.classList.replace("lune", "soleil");
+        icon1.classList.replace("lang-light", "lang-dark");
     } else {
         doc.classList.replace("dark", "light");
+        icon2.classList.replace("soleil", "lune");
+        icon1.classList.replace("lang-dark", "lang-light");
     }
 }
